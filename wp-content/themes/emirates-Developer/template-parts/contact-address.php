50f0adb1e79590966f4e2aba68c557b8c144dc3f
sahajeshe8 <sahajesh@nuox.io>
2025-12-15 18:50:16 +0530

    contact page

---
+++ wp-content/themes/emirates-Developer/template-parts/contact-address.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Contact Information Component Template
+ *
+ * @package tasheel
+ */
+
+?>
+
+<section class="pt_80 pb_80 bg_black bg_shadow">
+	<div class="wrap">
+       <ul class="contact_address_list">
+            <li>
+                <div class="contact_address_item">
+                    <h3>UB Emirates LLC - Branch</h3>
+                    <h4>Address</h4>
+
+                    <div class="address_block_item">
+                    <div class="contact_address_icon">
+                        <img src="http://localhost/emirates/wp-content/themes/emirates-Developer/assets/images/mark-white.svg" alt="Location">
+                    </div>
+
+                    <p>
+                    P.O. Box: 20827<br>
+Al Nahda -2,<br>
+Dubai,<br>
+United Arab Emirates<br>
+                    </p>
+
+</div>
+
+
+
+<div class="address_block_item">
+<div class="contact_address_icon">
+                        <img src="http://localhost/emirates/wp-content/themes/emirates-Developer/assets/images/phone-white.svg" alt="Phone">
+                    </div>
+
+                    <p>
+                        <a href="tel:+97143333333">+971 4 333 3333</a>
+                    </p>
+                    </div>
+
+
+                    <div class="address_block_item">
+                    <div class="contact_address_icon">
+                        <img src="http://localhost/emirates/wp-content/themes/emirates-Developer/assets/images/email-white.svg" alt="Email">
+                    </div>
+                    <p>
+                        <a href="mailto:info@example.com">info@example.com</a>
+                    </p></div>
+
+
+
+
+
+
+                </div>
+            </li>
+            
+
+
+
+
+
+
+
+
+
+
+            <li>
+                <div class="contact_address_item">
+                    <h3>UB Emirates LLC - Branch</h3>
+                    <h4>Address</h4>
+
+                    <div class="address_block_item">
+                    <div class="contact_address_icon">
+                        <img src="http://localhost/emirates/wp-content/themes/emirates-Developer/assets/images/mark-white.svg" alt="Location">
+                    </div>
+
+                    <p>
+                    P.O. Box: 20827<br>
+Al Nahda -2,<br>
+Dubai,<br>
+United Arab Emirates<br>
+                    </p>
+
+</div>
+
+
+
+<div class="address_block_item">
+<div class="contact_address_icon">
+                        <img src="http://localhost/emirates/wp-content/themes/emirates-Developer/assets/images/phone-white.svg" alt="Phone">
+                    </div>
+
+                    <p>
+                        <a href="tel:+97143333333">+971 4 333 3333</a>
+                    </p>
+                    </div>
+
+
+                    <div class="address_block_item">
+                    <div class="contact_address_icon">
+                        <img src="http://localhost/emirates/wp-content/themes/emirates-Developer/assets/images/email-white.svg" alt="Email">
+                    </div>
+                    <p>
+                        <a href="mailto:info@example.com">info@example.com</a>
+                    </p></div>
+
+
+
+
+
+
+                </div>
+            </li>
+
+
+
+            <li>
+                <div class="contact_address_item">
+                    <h3>UB Emirates LLC - Branch</h3>
+                    <h4>Address</h4>
+
+                    <div class="address_block_item">
+                    <div class="contact_address_icon">
+                        <img src="http://localhost/emirates/wp-content/themes/emirates-Developer/assets/images/mark-white.svg" alt="Location">
+                    </div>
+
+                    <p>
+                    P.O. Box: 20827<br>
+Al Nahda -2,<br>
+Dubai,<br>
+United Arab Emirates<br>
+                    </p>
+
+</div>
+
+
+
+<div class="address_block_item">
+<div class="contact_address_icon">
+                        <img src="http://localhost/emirates/wp-content/themes/emirates-Developer/assets/images/phone-white.svg" alt="Phone">
+                    </div>
+
+                    <p>
+                        <a href="tel:+97143333333">+971 4 333 3333</a>
+                    </p>
+                    </div>
+
+
+                    <div class="address_block_item">
+                    <div class="contact_address_icon">
+                        <img src="http://localhost/emirates/wp-content/themes/emirates-Developer/assets/images/email-white.svg" alt="Email">
+                    </div>
+                    <p>
+                        <a href="mailto:info@example.com">info@example.com</a>
+                    </p></div>
+
+
+
+
+
+
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+
---
+++ wp-content/themes/emirates-Developer/template-parts/contact-map.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Contact Map Component Template
+ *
+ * Static map section for the Contact Us page
+ *
+ * @package tasheel
+ */
+
+?>
+
+<section class="pt_80 pb_80 contact_map_section">
+	<div class="wrap">
+		<div class="contact_map_header">
+			<h3 class="h3_title_55 text_black">
+				Find Us on the Map
+			</h3>
+			<p>
+				Our offices are strategically located to serve you better across Dubai and the UAE.
+			</p>
+		</div>
+
+		<div class="contact_map_wrapper">
+			<div class="contact_map_image" aria-label="Map showing Dubai, Sharjah and surrounding areas"></div>
+		</div>
+	</div>
+</section>
+
+
